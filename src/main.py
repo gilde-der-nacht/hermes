13926be15826322a6f23d1e98768820098639207
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-02-24 20:49:26 +0100

    basic functionality

--- src/main.py
+++ src/main.py
@@ -7,10 +7,13 @@ load_dotenv()
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        if (message.author == self.user):
+            return
+        await message.add_reaction('✅')
+        await message.channel.send(f'Message received: "{message.content}"')
 
 
 client = MyClient()
